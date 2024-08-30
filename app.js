9a2e0b6080aa8b00ba0e6724034de96af8d589b0
ALIZAKHAN63 <alizakhan99163@gmail.com>
2024-08-30 13:42:01 +0500

    second

--- app.js
+++ app.js
@@ -9,30 +9,68 @@ function displayQuiz(){
     
     for (let key in question.Options) {
         html = html + '<p>' + key + ': ' + question.Options[key] +
-         ' <button onclick ="selectAnswer(\'' + key + '\')">✔️</button></p>';
+         ' <button onclick ="selectAnswer(\'' + key + '\',this)">✔️</button></p>';
     }
     
+    quizContainer.innerHTML = html;
+
+    html = html + '<button onclick="nextQuestion()">Next</button>';
+
     quizContainer.innerHTML = html;
     
     }
     
     displayQuiz();
+// q0
+    function selectAnswer(selectedkey,buttonElement) {
+        question = Questions[0];
 
-    function selectAnswer(selectedkey) {
-         question = Questions[0];
-        correctAnswerText = question.Options[question.CorrectAnswer];
+        correctAnswer = question.CorrectAnswer;
+
         if (selectedkey === question.CorrectAnswer) {
-            alert('Correct!');
+
+            buttonElement.parentElement.style.backgroundColor = 'green';
+
         } else {
-            alert('Wrong answer. The correct answer is ' + correctAnswerText + '.');
+            buttonElement.parentElement.style.backgroundColor = 'red';
         }
     }
+// q1
+function nextQuestion(){
+    const quizContainer = document.getElementById('quizcontainer');
+    const quizContainer1 = document.getElementById('quizcontainer1');
 
+    quizContainer.style.display = 'none';
+    quizContainer1.style.display = 'block';
 
-   
+    const question1 = Questions[1];
 
+    
+    
+    let html = '<h2>' + question1.Question + '</h2>';
 
+    for (let key in question1.Options) {
+        html = html + '<p>' + key + ': ' + question1.Options[key] +
+         ' <button onclick ="selectAnswer1(\'' + key + '\',this)">✔️</button></p>';
+    }
+    
+    quizContainer1.innerHTML = html;
 
+}
+// q1
+function selectAnswer1(selectedkey,buttonElement) {
+    question1 = Questions[1];
+
+    correctAnswer = question1.CorrectAnswer;
+
+    if (selectedkey === question1.CorrectAnswer) {
+
+        buttonElement.parentElement.style.backgroundColor = 'green';
+
+    } else {
+        buttonElement.parentElement.style.backgroundColor = 'red';
+    }
+}
 
 
 
--- data.js
+++ data.js
@@ -8,48 +8,25 @@ const Questions = [
             D: "Home Tool Markup Language"
         },
         CorrectAnswer: "A"
+    },
+    {
+        Question: "Which property is used to change the background color of an element in CSS?",
+        Options: {
+            A: "color",
+            B: "background-color",
+            C: "bgcolor",
+            D: "background"
+        },
+        CorrectAnswer: "B"
+    },
+    {
+        Question: "How do you write 'Hello, World!' in an alert box in JavaScript?",
+        Options: {
+            A: "alertBox('Hello, World!');",
+            B: "msgBox('Hello, World!');",
+            C: "alert('Hello, World!');",
+            D: "msg('Hello, World!');"
+        },
+        CorrectAnswer: "C"
     }
-]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     {
-//         Question: "Which property is used to change the background color of an element in CSS?",
-//         Options: {
-//             A: "color",
-//             B: "background-color",
-//             C: "bgcolor",
-//             D: "background"
-//         },
-//         CorrectAnswer: "B"
-//     },
-//     {
-//         Question: "How do you write 'Hello, World!' in an alert box in JavaScript?",
-//         Options: {
-//             A: "alertBox('Hello, World!');",
-//             B: "msgBox('Hello, World!');",
-//             C: "alert('Hello, World!');",
-//             D: "msg('Hello, World!');"
-//         },
-//         CorrectAnswer: "C"
-//     }
-// ];
+];
